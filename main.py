638a14227c2ca4884bf8528021a537aaf48dc1c5
Delidozit <jamesdelamore@hotmail.co.nz>
2024-12-30 22:04:05 +1300

    end of c2 l6 att: 1

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ def main():
 			if event.type == pygame.QUIT: # exit game on window close
 				return
 		screen.fill((0, 0 ,0)) # black screen
+		player.update(dt) # update players dt state
 		player.draw(screen) # draw the triangle for player
 		pygame.display.flip() # screen refresh
 		dt = clock.tick(60) / 1000 # setting clock speed to 60 fps
