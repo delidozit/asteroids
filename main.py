b958b6f7eb5c207c0fda934a4cb63a86e52ee431
Delidozit <jamesdelamore@hotmail.co.nz>
2025-01-02 08:30:55 +1300

    coppying boot.dev

--- main.py
+++ main.py
@@ -1,55 +1,61 @@
-import pygame
 import sys
-from constants import * # importing all from file
+import pygame
+from constants import *
 from player import Player
 from asteroid import Asteroid
-from asteroidfield import *
-from circleshape import CircleShape
+from asteroidfield import AsteroidField
 from shot import Shot
 
-updatable = pygame.sprite.Group()
-drawable = pygame.sprite.Group()
-Player.containers = (updatable, drawable)
-asteroids = pygame.sprite.Group()
-Asteroid.containers = (asteroids, updatable, drawable)
-AsteroidField.containers = (updatable)
-shots = pygame.sprite.Group()
-Shot.containers = (shots, updatable, drawable)
 
-def main(): 
-	pygame.init() 
-	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) # setting screen size
-	clock = pygame.time.Clock() #silly me... create a clock object
-	dt = 0
-	asteroid_field = AsteroidField()
-	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2) # player position
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
 
-	while True:
-		keys = pygame.key.get_pressed()
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT: # exit game on window close
-				return
-		if keys[pygame.K_SPACE]:
-				new_shot = player.shoot(dt)
-				if new_shot:
-					shots.add(new_shot)
-		screen.fill((0, 0 ,0)) # black screen
-		for sprite in updatable:
-			sprite.update(dt)
-		for asteroid in asteroids:  # iterate through your asteroids group
-			if player.check_collision(asteroid):  # check collision with each asteroid
-				print("Game over!")
-				sys.exit()
-		for shot in shots:
-			for asteroid in asteroids:
-				if asteroid.check_collision(shot):
-					shot.kill()
-					asteroid.kill()
-		for sprite in drawable:
-			sprite.draw(screen)
-		pygame.display.flip() # screen refresh
-		dt = clock.tick(60) / 1000 # setting clock speed to 60 fps
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    Shot.containers = (shots, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
+
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    dt = 0
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        for obj in updatable:
+            obj.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                print("Game over!")
+                sys.exit()
+
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    shot.kill()
+                    asteroid.split()
+
+        screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)
+
+        pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
-	main()
+    main()
--- player.py
+++ player.py
@@ -1,14 +1,18 @@
 import pygame
+from constants import *
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, PLAYER_SHOOT_SPEED, PLAYER_SPEED, PLAYER_TURN_SPEED, PLAYER_SHOOT_COOLDOWN
 from shot import Shot
+
 
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
-        self.timer = 0.0
-    # in the player class
+        self.shoot_timer = 0
+
+    def draw(self, screen):
+        pygame.draw.polygon(screen, "white", self.triangle(), 2)
+
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
@@ -16,42 +20,32 @@ class Player(CircleShape):
         b = self.position - forward * self.radius - right
         c = self.position - forward * self.radius + right
         return [a, b, c]
-    # boot.dev copy paste for drawing triangle
-
-    def rotate(self, dt):
-        self.rotation += PLAYER_TURN_SPEED * dt
-
-    def shoot(self, dt):
-        if self.timer <= 0:
-            self.timer = PLAYER_SHOOT_COOLDOWN
-            shot_direction = pygame.Vector2(0, -1)
-            shot_direction = shot_direction.rotate(self.rotation + 180)
-            shot_velocity = shot_direction * PLAYER_SHOOT_SPEED
-            new_shot = Shot(self.position.x, self.position.y, shot_velocity)
-            return new_shot
-        else:
-            print("cannot shoot yet")
 
     def update(self, dt):
+        self.shoot_timer -= dt
         keys = pygame.key.get_pressed()
 
-        if keys[pygame.K_a]:
-            self.rotate(-dt)
-        if keys[pygame.K_d]:
-            self.rotate(dt)
         if keys[pygame.K_w]:
             self.move(dt)
         if keys[pygame.K_s]:
             self.move(-dt)
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
         if keys[pygame.K_SPACE]:
-            self.shoot(dt)
-        if self.timer > 0:
-            self.timer -= dt
+            self.shoot()
+
+    def shoot(self):
+        if self.shoot_timer > 0:
+            return
+        self.shoot_timer = PLAYER_SHOOT_COOLDOWN
+        shot = Shot(self.position.x, self.position.y)
+        shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
 
     def move(self, dt):
-        forward = pygame.Vector2(0, 1).rotate(self.rotation) # forward vector
-        self.position += forward * PLAYER_SPEED * dt # position update
-        
-
-    def draw(self, screen):
-        pygame.draw.polygon(screen, "white", self.triangle(), 2)
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
